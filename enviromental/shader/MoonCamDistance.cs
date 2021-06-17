a09c5a3d1c84e649de9fb3a1038b2fd64f33f08f
LenZ <eugene@musegames.com>
2021-06-17 02:15:31 -0400

    Update MoonCamDistance.cs

--- enviromental/shader/MoonCamDistance.cs
+++ enviromental/shader/MoonCamDistance.cs
@@ -9,11 +9,11 @@ public class MoonCamDistance : MonoBehaviour {
     public Camera ccam;
     // Update is called once per frame
    void LateUpdate () {
-        if (ccam == null)
-        {
+       // if (ccam == null)
+        //{
             if (Camera.main != null) ccam = Camera.main;
             else if (Camera.current != null) ccam = Camera.current;
-        }
+        //}
 
         if (ccam != null) {
             transform.position = ccam.transform.position + (-master.transform.forward * distance);
