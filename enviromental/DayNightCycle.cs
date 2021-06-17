b3e19b00ed6db0d4c33aeb5dc9a519ed55f3585a
LenZ <eugene@musegames.com>
2021-06-17 19:55:57 -0400

    smooth transition in setup

--- enviromental/DayNightCycle.cs
+++ enviromental/DayNightCycle.cs
@@ -17,7 +17,9 @@ public class DayNightCycle : MonoBehaviour {
     public Light moonLight;
     public GameObject moonLightGo;
 
-
+    [Range(0,1)]
+    public float smoothLerp =0.5f;
+    public float GetSmoothLerp { get { if (Application.isPlaying || smoothLerp <= 0) return smoothLerp *Time.deltaTime; else return 1; } }
 
     [System.Serializable]
     public struct SkyData
@@ -30,10 +32,12 @@ public class DayNightCycle : MonoBehaviour {
         public AnimationCurve sunSizeOverTime;
         [Space()]
         public Gradient dayTopColorOverTime;
-        public Gradient dayColorOverTime;
+        public Gradient dayMidColorOverTime;
+
         [Space()]
         public Gradient nightTopColorOverTime;
-        public Gradient nightColorOverTime;
+        public Gradient nightMidColorOverTime;
+ 
         [Space()]
         public Gradient dayFogOverTime;
         [Range(0, 1)]
@@ -52,10 +56,10 @@ public class DayNightCycle : MonoBehaviour {
     [Range(0, 1)]
     public float timeOfDay = 0;
 
-    [HideInInspector()]
+    //[HideInInspector()]
     [Range(0, 1)]
-    public float fadeNight;
-    [HideInInspector()]
+    public float fadeToNight;
+   // [HideInInspector()]
     [Range(0, 1)]
     public float smoothFadeNight;
 
@@ -75,7 +79,8 @@ public class DayNightCycle : MonoBehaviour {
             return;
         }
     }
-
+    Material ClonedMat;
+    Material prevSourceMat;
     public void Setup()
     {
         RenderSettings.sun = sunLight;
@@ -84,7 +89,11 @@ public class DayNightCycle : MonoBehaviour {
         print("changed skymat");
         if (Application.isPlaying)
         {
-            Material ClonedMat = Instantiate(skyData.skyBoxMat);//new Material(skyData.skyBoxMat);
+            if (skyData.skyBoxMat != prevSourceMat)
+            {
+                ClonedMat = Instantiate(skyData.skyBoxMat);//new Material(skyData.skyBoxMat);
+                prevSourceMat = skyData.skyBoxMat;
+            }
             RenderSettings.skybox = ClonedMat;
         }
         else
@@ -108,7 +117,10 @@ public class DayNightCycle : MonoBehaviour {
     }
     public void TimeOfDayBar()
     {
-        if (Application.isPlaying) timeOfDay += Time.deltaTime * (skyData.speed / 360);
+        if (Application.isPlaying)
+        {
+            timeOfDay += Time.deltaTime * (skyData.speed / 360);
+        }
         //repeat
         if (timeOfDay > 1) timeOfDay = timeOfDay - 1;
         else if (timeOfDay < 0) timeOfDay = timeOfDay + 1;
@@ -136,49 +148,30 @@ public class DayNightCycle : MonoBehaviour {
 
         //rotate transform
         SunRot.x = timeOfDay * 360;
-        sunLight.transform.localEulerAngles = SunRot;
+        sunLight.transform.rotation =Quaternion.Slerp(sunLight.transform.rotation, Quaternion.Euler(SunRot), GetSmoothLerp);
 
         //visuals
         if (active_DayNight)
         {
-            //shuts lights as it goes over the horizon
-            if (dayLightTime <= 0 || dayLightTime >= 1)
-            {
-                //day to night time========================================= ☽︎
-                if (fadeNight >= 1)
-                {
-                    sunLight.enabled = false;
-                }
-                else
-                {
-                    sunLight.enabled = true;
-                }
-                sunLight.intensity = Mathf.Lerp(skyData.lightIntensity, 0, fadeNight);
+            float _fadeIntensity = Mathf.Lerp(0, 1, fadeToNight);
+            float _fadeIntensity_invert = Mathf.Lerp(1, 0, fadeToNight);
 
-                moonLight.enabled = true;
-                moonLight.intensity = Mathf.Lerp(0, skyData.moonLightIntensity, fadeNight);
-            }
-            else
-            {
-                //night to day time=========================================== ☼
+            //fade sun light
+            bool AtDay = (fadeToNight < 1);
+            sunLight.enabled = AtDay;
 
-                if (fadeNight <= 0)
-                {
-                    moonLight.enabled = false;
-                }
-                else
-                {
-                    moonLight.enabled = true;
-                }
-                moonLight.intensity = Mathf.Lerp(0, skyData.moonLightIntensity, fadeNight);
+            float L_SunLightIntensity = Mathf.Lerp(sunLight.intensity, skyData.lightIntensity, GetSmoothLerp) * _fadeIntensity_invert;
+            sunLight.intensity = L_SunLightIntensity;
 
-                sunLight.enabled = true;
-                sunLight.intensity = Mathf.Lerp(skyData.lightIntensity, 0, fadeNight);
+            //fade moon light
+            bool AtNight = (fadeToNight > 1);
+            moonLight.enabled = AtNight;
 
-            }
+            float L_MoonLightIntensity = Mathf.Lerp(moonLight.intensity, skyData.moonLightIntensity, GetSmoothLerp) * _fadeIntensity;
+            moonLight.intensity = L_MoonLightIntensity;
+            //
 
             SkyColor();
-
             moonLightGo.SetActive(true);
         }
         else
@@ -200,38 +193,63 @@ public class DayNightCycle : MonoBehaviour {
     {
         SmoothNight();
         //change direcrtional color on light over time
-        sunLight.color = skyData.dayColorOverTime.Evaluate(dayLightTime);
-        moonLight.color = skyData.nightColorOverTime.Evaluate(nightLightTime);
+
+        sunLight.color = Color.Lerp(sunLight.color, skyData.dayMidColorOverTime.Evaluate(dayLightTime), GetSmoothLerp);
+        moonLight.color = Color.Lerp(moonLight.color, skyData.nightMidColorOverTime.Evaluate(nightLightTime), GetSmoothLerp);
         Color _dayFogOverTime;
 
         if (dayLightTime > 0)
         {
             //_topColor
-            if (RenderSettings.skybox.HasProperty(_topColor)) RenderSettings.skybox.SetColor(_topColor, (skyData.dayTopColorOverTime.Evaluate(dayLightTime)));
+            if (RenderSettings.skybox.HasProperty(_topColor))
+            {
+                Color L_topColor = Color.Lerp(RenderSettings.skybox.GetColor(_topColor), (skyData.dayTopColorOverTime.Evaluate(dayLightTime)), GetSmoothLerp);
+                RenderSettings.skybox.SetColor(_topColor, L_topColor);
+            }
             //_midColor
-            if (RenderSettings.skybox.HasProperty(_midColor)) RenderSettings.skybox.SetColor(_midColor, skyData.dayColorOverTime.Evaluate(dayLightTime));
+            if (RenderSettings.skybox.HasProperty(_midColor))
+            {
+                Color L_midColor = Color.Lerp(RenderSettings.skybox.GetColor(_midColor), (skyData.dayMidColorOverTime.Evaluate(dayLightTime)), GetSmoothLerp);
+                RenderSettings.skybox.SetColor(_midColor, L_midColor);
+            }
 
-
-            _dayFogOverTime = Color.Lerp(skyData.dayFogOverTime.Evaluate(dayLightTime), skyData.dayColorOverTime.Evaluate(dayLightTime), skyData.fogToDayColor);
+            _dayFogOverTime = Color.Lerp(skyData.dayFogOverTime.Evaluate(dayLightTime), skyData.dayMidColorOverTime.Evaluate(dayLightTime), skyData.fogToDayColor);
         }
         else
         {
             //_topColor
-            if (RenderSettings.skybox.HasProperty(_topColor)) RenderSettings.skybox.SetColor(_topColor, (skyData.nightTopColorOverTime.Evaluate(nightLightTime)));
+            if (RenderSettings.skybox.HasProperty(_topColor))
+            {
+                Color L_topColor = Color.Lerp(RenderSettings.skybox.GetColor(_topColor), (skyData.nightTopColorOverTime.Evaluate(nightLightTime)), GetSmoothLerp);
+                RenderSettings.skybox.SetColor(_topColor, L_topColor);
+            }
             //_midColor
-            if (RenderSettings.skybox.HasProperty(_midColor)) RenderSettings.skybox.SetColor(_midColor, skyData.nightColorOverTime.Evaluate(nightLightTime));
-
-            _dayFogOverTime = Color.Lerp(skyData.dayFogOverTime.Evaluate(dayLightTime), skyData.nightColorOverTime.Evaluate(nightLightTime), skyData.fogToDayColor);
+            if (RenderSettings.skybox.HasProperty(_midColor))
+            {
+                Color L_midColor = Color.Lerp(RenderSettings.skybox.GetColor(_midColor), (skyData.nightMidColorOverTime.Evaluate(nightLightTime)), GetSmoothLerp);
+                RenderSettings.skybox.SetColor(_midColor, L_midColor);
+            }
+            _dayFogOverTime = Color.Lerp(skyData.dayFogOverTime.Evaluate(dayLightTime), skyData.nightMidColorOverTime.Evaluate(nightLightTime), skyData.fogToDayColor);
         }
 
-
-        RenderSettings.fogColor = _dayFogOverTime;
-        //_bottomColor
-        if (RenderSettings.skybox.HasProperty(_bottomColor)) RenderSettings.skybox.SetColor(_bottomColor, _dayFogOverTime);
-
+        if (RenderSettings.skybox.HasProperty(_bottomColor))
+        {
+            //_bottomColor
+            RenderSettings.fogColor = Color.Lerp(RenderSettings.fogColor, _dayFogOverTime, GetSmoothLerp);
+            RenderSettings.skybox.SetColor(_bottomColor, RenderSettings.fogColor);
+        }
         //nightsky
-        if (RenderSettings.skybox.HasProperty(_nightOpacity)) RenderSettings.skybox.SetFloat(_nightOpacity, smoothFadeNight);
-        if (RenderSettings.skybox.HasProperty(_sunScale)) RenderSettings.skybox.SetFloat(_sunScale, skyData.sunSizeOverTime.Evaluate(timeOfDay));
+        if (RenderSettings.skybox.HasProperty(_nightOpacity))
+        {
+            float L_nightOpacity = Mathf.Lerp(RenderSettings.skybox.GetFloat(_nightOpacity), smoothFadeNight, GetSmoothLerp);
+            RenderSettings.skybox.SetFloat(_nightOpacity, L_nightOpacity);
+        }
+        if (RenderSettings.skybox.HasProperty(_sunScale))
+        {
+            //sunscale
+            float L_sunScale = Mathf.Lerp(RenderSettings.skybox.GetFloat(_sunScale), skyData.sunSizeOverTime.Evaluate(timeOfDay), GetSmoothLerp);
+            RenderSettings.skybox.SetFloat(_sunScale, L_sunScale);
+        }
 
     }
     public void SmoothNight()
@@ -239,15 +257,15 @@ public class DayNightCycle : MonoBehaviour {
         //smooth the day night lerp
         if (nightLightTime >= 0.95f)
         {
-            fadeNight = Mathf.Lerp(1, 0, (nightLightTime - 0.95f) / 0.05f);
+            fadeToNight = Mathf.Lerp(1, 0, (nightLightTime - 0.95f) / 0.05f);
         }
         else if (nightLightTime <= 0.05f)
         {
-            fadeNight = Mathf.Lerp(0, 1, nightLightTime / 0.05f);
+            fadeToNight = Mathf.Lerp(0, 1, nightLightTime / 0.05f);
         }
         else
         {
-            fadeNight = 1;
+            fadeToNight = 1;
         }
 
         //smooth the day night lerp
