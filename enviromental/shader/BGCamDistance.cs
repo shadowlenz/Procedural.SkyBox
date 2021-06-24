b0a199887cabfa0df64f789225f06619e9baab58
LenZ <eugene@musegames.com>
2021-06-24 13:44:14 -0400

    polished

--- enviromental/shader/BGCamDistance.cs
+++ enviromental/shader/BGCamDistance.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoonCamDistance : MonoBehaviour {
+public class BGCamDistance : MonoBehaviour {
 
     public Transform master;
     public float distance = 1000;
